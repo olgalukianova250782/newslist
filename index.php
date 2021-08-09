4d8e293db2d3e83a102db53f41702002321fd12e
o.lukianova <okondrashova@bk.ru>
2021-08-10 00:14:00 +0300

    Небольшие правки файла шаблона

--- index.php
+++ index.php
@@ -2,19 +2,7 @@
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
     die();
 }
-$APPLICATION->SetTitle("Новости компании");
-$APPLICATION->SetPageProperty("description", "Комиссионная продажа техники!");
-/** @var array $arParams */
-/** @var array $arResult */
-/** @global CMain $APPLICATION */
-/** @global CUser $USER */
-/** @global CDatabase $DB */
-/** @var CBitrixComponentTemplate $this */
-/** @var string $templateName */
-/** @var string $templateFile */
-/** @var string $templateFolder */
-/** @var string $componentPath */
-/** @var CBitrixComponent $component */
+
 $this->setFrameMode(true);
 ?>
 <link rel="stylesheet" href="/style.css">
